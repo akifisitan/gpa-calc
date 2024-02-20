916da4873ddce489172d278a2e43a7e4f4bbee72
akifisitan <102677971+akifisitan@users.noreply.github.com>
2024-02-20 23:15:26 +0300

    deps: add shadcn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import "../app.pcss";
+	import { Toaster } from "$lib/components/ui/sonner";
+	import { ModeWatcher } from "mode-watcher";
 </script>
+
+<svelte:head>
+	<title>GPA Calculator</title>
+</svelte:head>
+
+<ModeWatcher />
+
+<Toaster />
 
 <slot />
